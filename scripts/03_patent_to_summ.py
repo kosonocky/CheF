e8fc9fff0734b130dd4bf4b23e454ae46cdae153
Clay Kosonocky <90992096+kosonocky@users.noreply.github.com>
2023-06-30 10:44:06 -0500

    Decent version

--- scripts/03_patent_to_summ.py
+++ scripts/03_patent_to_summ.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 import requests
@@ -8,8 +9,11 @@ from itertools import repeat
 import pandas as pd
 from pathlib import Path
 from bs4 import BeautifulSoup
+from bs4.element import Comment
+from unidecode import unidecode
 
 import openai
+import tiktoken
 
 
 def google_patents_api_call(patent_id, _restarts = 0, _original_patent_id = ""):
@@ -73,6 +77,55 @@ def google_patents_api_call(patent_id, _restarts = 0, _original_patent_id = ""):
     return response
 
 
+def remove_non_utf(text):
+    cleaned_text = ''
+    for char in text:
+        try:
+            char.encode('utf-8')
+            cleaned_text += char
+        except UnicodeEncodeError:
+            continue
+    return cleaned_text
+
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+def fix_text(text):
+    # encode greek letters to latin and back to utf-8
+    text = text.encode('latin-1', 'ignore').decode('utf-8', 'ignore')
+
+    pattern = re.compile(r'[a-zA-Z0-9\s\(\)\[\]{}\.,;\:\'\-\\\/\+\*\^=\?~`!@#$%&|<>\u0391-\u03C9]+')
+    text = ' '.join(pattern.findall(text))
+    # text = re.sub(r"[\[\]\(\)\{\}\.,;:]+", "", text)
+    text = re.sub(r"(\s\w\s)+", " ", text)
+    text = text.replace('\n', ' ').replace('\t', ' ').replace('\r', ' ')
+    text = ' '.join(text.split())
+
+    # remove first occurance of "Abstract" and "Description"
+    text = re.sub(r"Abstract", "", text, count=1)
+    text = re.sub(r"Description", "", text, count=1)
+    # text = unidecode(text)
+
+
+    # remove patterns of symbols spaced by a space, following space char space char space ...
+    pattern = re.compile(r'(\s[\(\)\[\]{}\.,;\:\'\-\\\/\+\*\^=\?~`!@#$%&|<>]+\s)+')
+    text = pattern.sub(' ', text)
+
+    # remove patterns like ) ) ) ) ) ) ) ) ) ) ) ) ) ) ) ) (alky)) ) ) ) ) ) ) ) ) ) ) )
+    pattern = re.compile(r'(\)\s)+')
+    text = pattern.sub(') ', text)
+
+    
+    # remove repeated characters separated by a space, following space char space char space ...
+
+    return text
+
+
 def get_patent_info(patent_ids):
     """
     Gets patent abstracts and descriptions for each patent in a set of patent IDs.
@@ -92,34 +145,34 @@ def get_patent_info(patent_ids):
     """
     
     patent_info = dict()
-    if len(patent_ids) > 0:
-        for patent_id in patent_ids:
-            try:
-                # remove dashes from patent_id
-                response = google_patents_api_call(patent_id = patent_id.replace("-", ""))
-                if response.ok:
-                    # Scrape patent abstract and description
-                    html_content = response.text
-                    soup = BeautifulSoup(html_content, 'html.parser')
-                    abstract = soup.find('div', class_='abstract')
-                    description = soup.find('section', {'itemprop': 'description'})
+    if len(patent_ids) == 0:
+        return patent_info
+    for patent_id in patent_ids:
+        try:
+            # remove dashes from patent_id
+            response = google_patents_api_call(patent_id = patent_id.replace("-", ""))
+            if response.ok:
+                # Scrape patent abstract and description
+                html_content = response.text
+                soup = BeautifulSoup(html_content, 'html.parser')
+                abstract = soup.find('div', class_='abstract')
+                description = soup.find('section', {'itemprop': 'description'})
 
-                    if abstract is None:
-                        abstract = "None"
-                    else:
-                        abstract = abstract.text.replace('\n', ' ')
+                if abstract is None:
+                    abstract = "None"
+                else:
+                    abstract = fix_text(abstract.text)
 
-                    if description is None:
-                        description = "None"
-                    else:
-                        description = description.text.replace('\n', ' ')
-                    
-                    patent_info.update({patent_id: {"abstract": abstract, "description": description}})
-            except Exception as e:
-                print(patent_id, e)
+                if description is None:
+                    description = "None"
+                else:
+                    description = fix_text(description.text)
+
+                
+                patent_info.update({patent_id: {"abstract": abstract, "description": description}})
+        except Exception as e:
+            print(patent_id, e)
     return patent_info
-
-
 
 
 def chatgpt(api_key, model = "gpt-3.5-turbo", system_prompt = "", user_prompt = "", gpt_temperature=0):
@@ -149,6 +202,7 @@ def chatgpt(api_key, model = "gpt-3.5-turbo", system_prompt = "", user_prompt = 
         Generated text.
     """
     if (user_prompt == "") or (system_prompt == "") or (api_key == "") or (model == ""):
+        print("EXITING HERE")
         return ""
     api_success = False
     tries = 0
@@ -169,6 +223,7 @@ def chatgpt(api_key, model = "gpt-3.5-turbo", system_prompt = "", user_prompt = 
             time.sleep(5)
             if tries > 10:
                 print(e)
+                print(user_prompt)
                 return "API REQUEST ERROR"
             else:
                 pass
@@ -216,11 +271,24 @@ def summarization_wrapper(patent_info, api_key, model = "gpt-3.5-turbo", system_
             user_prompt_complete = f"{user_prompt}\n\nAbstract:\n{value['abstract']}\n\nDescription:\n{value['description'][:desc_len]}"
         else:
             user_prompt_complete = f"{user_prompt}\n\nAbstract:\n{value['abstract']}\n\nDescription:\n{value['description']}"
+
+        # ensure less than 4k tokens. Most are ~1k
+        enc = tiktoken.get_encoding("cl100k_base")
+        tok = enc.encode(user_prompt_complete)
+        user_prompt_complete = enc.decode(tok[:4000])
+        
+        # print("*********")
+        # print(key)
+        # print(user_prompt_complete)
+        # summ = ""
+        
         summ = chatgpt(api_key = api_key,
                         model = model,
                         system_prompt = system_prompt,
                         user_prompt = user_prompt_complete,
                         gpt_temperature = gpt_temperature)
+        # print(summ)
+        # print("*********")
         if summ == "API REQUEST ERROR":
             print(f"API REQUEST ERROR FOR {key}. FAILED 10 TIMES.")
         summarizations.update({key: summ})
@@ -252,10 +320,8 @@ def summarizations_to_str(summarizations):
     # flatten list
     s_list = [item for sublist in s_list for item in sublist]
     # remove empty strings and 'NA', and "API REQUEST ERROR"
-    s_list = [s for s in s_list if s not in ['', 'NA', 'API REQUEST ERROR']]
+    s_list = [s for s in s_list if s not in ['', 'NA', 'API REQUEST ERROR', " NA"]]
     return str(set(s_list))
-
-
 
 
 def main():
@@ -268,16 +334,19 @@ def main():
     gpt_model = "gpt-3.5-turbo"
     api_key = "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"
     gpt_summ_system_prompt = "You are an organic chemist summarizing chemical patents"
-    gpt_summ_user_prompt = "Output a brief list of 1-3 word phrases that best describe the chemical and pharmacological function(s) of the molecule described by the given patent abstract and description. Be specific, concise, and follow the syntax 'descriptor_1 / descriptor_2 / etc', writing 'NA' if nothing is provided. The following is the patent: "
     openai.api_key = api_key
+    # gpt_summ_user_prompt = r"Return a short set of three 1-3 word descriptors that best describe the chemical or pharmacological function(s) of the molecule described by the given patent abstract and partial description (giving more weight to abstract). Be specific and concise, but not necessarily comprehensive (choose a small number of great descriptor, avoiding common words). Follow the syntax '{descriptor_1} / {descriptor_2} / {etc}', writing 'NA' if nothing is provided. DO NOT BREAK THIS SYNTAX. The following is the patent:"
+    gpt_summ_user_prompt = r"Return a short set of three 1-3 word descriptors that best describe the chemical or pharmacological function(s) of the molecule described by the given patent abstract and partial description (giving more weight to abstract). Be specific and concise, but not necessarily comprehensive (choose a small number of great descriptor). Follow the syntax '{descriptor_1} / {descriptor_2} / {etc}', writing 'NA' if nothing is provided. DO NOT BREAK THIS SYNTAX. The following is the patent:"
 
     df = pd.read_csv('../data/surechembl_smiles_canon_chiral_randomized_patents_l10p_noNA.csv', nrows=head_n)
     df["patent_ids"] = df["patent_ids"].map(literal_eval)
     print(f"Time to read in data: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds.")
 
+    # df = df.iloc[76:77]
+    # df = df.iloc[90:100]
+
     n_cpus = 12
     print(f"INFO: Using {n_cpus} CPUs")
-    t0 = time.time()
     with mp.Pool(n_cpus) as p:
         patent_info = p.map(get_patent_info, df["patent_ids"].tolist())
         print(f"Time to get patent info: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds.")
@@ -286,7 +355,20 @@ def main():
         print(f"Time to get summarizations: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds.")
     df["summarization_sources"] = [s for s in summarization_sources]
     df["summarizations"] = [s for s in summarizations]
-    df[["smiles", "cid", "patent_ids", "summarization_sources", "summarizations"]].to_csv(f"{output_dir}/surechembl_summarizations_top-{head_n}_{gpt_model}_desc-{desc_len}.csv", index=True)
+
+    # append number to end of save path to avoid overwriting
+    i = 1
+    save_path = f"{output_dir}/surechembl_summarizations_top-{head_n}_{gpt_model}_desc-{desc_len}_{i}"
+    if Path(f"{save_path}.csv").exists():
+        while Path(f"{save_path}.csv").exists():
+            save_path = f"{output_dir}/surechembl_summarizations_top-{head_n}_{gpt_model}_desc-{desc_len}_{i}"
+            i += 1
+
+    df[["smiles", "cid", "patent_ids", "summarization_sources", "summarizations"]].to_csv(f"{save_path}.csv", index=False)
+
+    # save prompt
+    with open(f"{save_path}_prompt.txt", "w") as f:
+        f.write(gpt_summ_system_prompt + "\n\n" + gpt_summ_user_prompt)
     print(f"Time to save: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds.")
 
 
