290f7fe9af20c107895e73114b1d5c16dc40cdd1
Clay Kosonocky <90992096+kosonocky@users.noreply.github.com>
2023-07-14 11:29:25 -0500

    removed pandarallel and replaced with map

--- scripts/01_smi_to_cid.py
+++ scripts/01_smi_to_cid.py
@@ -1,14 +1,8 @@
 import time
 import pandas as pd
 
-from pandarallel import pandarallel
 from rdkit import Chem
 from collections import defaultdict
-
-
-pandarallel.initialize(progress_bar=False)
-
-# NOTE Need to update to remove pandarallel and instead map on index. Wayyyy faster that way.
 
 def mol_to_inchi_key(smi):
     try:
@@ -24,7 +18,10 @@ def main():
     df = df.rename(columns={"SMILES": "smiles"})
     print(f"Loaded smiles into dataframe. Time: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds\n")
 
-    df['inchi_key'] = df['smiles'].parallel_apply(mol_to_inchi_key)
+    df = df.set_index("smiles")
+    df['inchi_key'] = df.index.map(lambda x: mol_to_inchi_key(x))
+    df["smiles"] = df.index
+
     df = df[df['inchi_key'] != None].reset_index(drop=True)
     print(f"smiles mapped to inchi_key. Time: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds\n")
     
@@ -35,7 +32,10 @@ def main():
             inchi_to_cid[key] = val
     print(f"Loaded CID-InChI-key dictionary. Time: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds\n")
 
-    df['cid'] = df['inchi_key'].parallel_apply(lambda x: inchi_to_cid[x] if x in inchi_to_cid else None)
+
+    df = df.set_index("inchi_key")
+    df['cid'] = df.index.map(lambda x: inchi_to_cid[x] if x in inchi_to_cid else None)
+
     df = df[df['cid'] != None].dropna().reset_index(drop=True)
     print(f"Mapped cid to inchi_key. Time: {round(abs((t_old:=t_curr) - (t_curr:=time.time())), 3)} seconds\n")
 
