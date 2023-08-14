379c07520eac2d2b78757844541b121b392d517a
Clay Kosonocky <90992096+kosonocky@users.noreply.github.com>
2023-08-14 12:54:11 -0500

    working version

--- scripts/models/train_fp_nn.py
+++ scripts/models/train_fp_nn.py
@@ -17,7 +17,7 @@ from torch.optim import Adam
 def load_data(df_path):
     print("Creating dataset...")
     df = pd.read_pickle(df_path)
-    data = df[['fingerprint', 'summarizations']]
+    data = df[['fingerprint', 'summarizations', "cid"]]
     data["summarizations"] = data["summarizations"].map(set)
 
     # create set of all labels
@@ -46,7 +46,10 @@ def load_data(df_path):
     X = scaler.fit_transform(X)
     print(f"X.shape: {X.shape}")
 
-    return X, y, mlb
+    # save cid
+    cid = data["cid"].values
+
+    return X, y, cid, mlb
 
 
 
@@ -96,7 +99,7 @@ def create_dataloader(X, y, batch_size=32):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return dataloader
 
-def train(model, X, y, lr_rate=0.001, epochs=3, batch_size=32, kfolds=5, save_path = "", device="cpu"):
+def train(model, X, y, lr_rate=0.001, epochs=10, batch_size=32, kfolds=10, save_path = "", device="cpu"):
     print("Training model...")
     kf = KFold(n_splits=kfolds, shuffle=True, random_state=42)
     optimizer = Adam(model.parameters(), lr=lr_rate)
@@ -104,11 +107,14 @@ def train(model, X, y, lr_rate=0.001, epochs=3, batch_size=32, kfolds=5, save_pa
 
     loss_func = nn.BCEWithLogitsLoss()
 
+    results_df = pd.DataFrame(columns=["fold", "epoch", "train_loss", "valid_loss"])
+
     for fold, (train, valid) in enumerate(kf.split(X, y)):
         losses = []
-        checkpoint_losses_train = []
+        # checkpoint_losses_train = []
 
         for epoch in range(epochs):
+            avg_epoch_train_loss = 0
             # print("\nTraining fold", fold, "epoch", epoch+1)
             for i, (batch_X, batch_y) in enumerate(create_dataloader(X[train], y[train], batch_size=batch_size)):
                 batch_X = batch_X.to(device)
@@ -118,6 +124,7 @@ def train(model, X, y, lr_rate=0.001, epochs=3, batch_size=32, kfolds=5, save_pa
                 outputs = model(batch_X)
                 loss = multilabel_loss(loss_func, outputs, batch_y)
                 losses.append(loss.item())
+                avg_epoch_train_loss += loss.item()
 
                 # backward pass
                 optimizer.zero_grad()
@@ -126,10 +133,12 @@ def train(model, X, y, lr_rate=0.001, epochs=3, batch_size=32, kfolds=5, save_pa
 
                 if (i+1) % 100 == 0:
                     print(f"Train: Fold {fold}, Epoch {epoch+1}/{epochs}, Batch {i+1}/{len(train)//batch_size}, Loss {loss.item():.4f}")
-                    checkpoint_losses_train.append(loss.item())
+                    # checkpoint_losses_train.append(loss.item())
+            avg_epoch_train_loss /= len(train)//batch_size
 
             # eval on test with batchloader
-            checkpoint_losses_valid = []
+            avg_epoch_valid_loss = 0
+            # checkpoint_losses_valid = []
             for i, (batch_X, batch_y) in enumerate(create_dataloader(X[valid], y[valid], batch_size=batch_size)):
                 batch_X = batch_X.to(device)
                 batch_y = batch_y.to(device)
@@ -138,19 +147,66 @@ def train(model, X, y, lr_rate=0.001, epochs=3, batch_size=32, kfolds=5, save_pa
                     outputs = model(batch_X)
                     # forward pass
                     loss = multilabel_loss(loss_func, outputs, batch_y)
-                    checkpoint_losses_valid.append(loss.item())
-                    
-            print(f"\nValidation: Fold {fold}, Epoch {epoch+1}/{epochs}, Mean Loss {np.mean(checkpoint_losses_valid):.4f}")
+                    # checkpoint_losses_valid.append(loss.item())
+                    #         
+                avg_epoch_valid_loss += loss.item()
+            avg_epoch_valid_loss /= len(valid)//batch_size
+            print(f"\nValidation: Fold {fold}, Epoch {epoch+1}/{epochs}, Mean Loss {loss.item():.4f}\n")
 
+            # save results to df
+            results_df = results_df.append({"fold": fold, "epoch": epoch+1, "train_loss": avg_epoch_train_loss, "valid_loss": avg_epoch_valid_loss}, ignore_index=True)
+            
             # save model each epoch
             torch.save(model.state_dict(), f"{save_path}/fold_{fold}_epoch_{epoch+1}.pth")
 
-    return losses, checkpoint_losses_train, checkpoint_losses_valid
+    return losses, results_df
 
 
-        
 
 
+def test(model, X, y, cid, mlb, epoch, kfold, batch_size=32, device="cpu", save_path=""):
+    """
+    Test set eval that saves results to csv (with cid, pred, target, and converted labels)
+
+    """
+
+    print("Testing model...")
+    loss_func = nn.BCEWithLogitsLoss()
+    model.eval()
+    losses = []
+    preds = []
+    targets = []
+    cids = []
+    for i, (batch_X, batch_y, batch_cid) in enumerate(create_dataloader(X, y, cid, batch_size=batch_size)):
+        batch_X = batch_X.to(device)
+        batch_y = batch_y.to(device)
+
+        with torch.no_grad():
+            outputs = model(batch_X)
+            # forward pass
+            loss = multilabel_loss(loss_func, outputs, batch_y)
+            losses.append(loss.item())
+
+            # save preds and targets
+            preds.append(outputs.cpu().numpy())
+            targets.append(batch_y.cpu().numpy())
+
+            # save cid
+            cids.append(batch_cid)
+
+    preds = np.vstack(preds)
+    targets = np.vstack(targets)
+    cids = np.hstack(cids)
+
+    # convert preds and targets to labels
+    preds_labels = mlb.inverse_transform(preds)
+    targets_labels = mlb.inverse_transform(targets)
+
+    # save results to csv
+    results = pd.DataFrame({"cid": cids, "preds": preds, "targets": targets, "preds_labels": preds_labels, "targets_labels": targets_labels})
+    results.to_csv(save_path / "test_results.csv", index=False)
+
+    
     
 
 
@@ -159,20 +215,26 @@ def main():
     save_path = Path("models/fp_nn")
     save_path.mkdir(parents=True, exist_ok=True)
     
-    X, y, mlb = load_data(df_path)
+    X, y, cid, mlb = load_data(df_path)
 
     # hold out test set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+    X_train, X_test, y_train, y_test, cid_train, cid_test = train_test_split(X, y, cid, test_size=0.1, random_state=42)
     
     model, device = load_model_device()
-    losses, checkpoint_losses_train, checkpoint_losses_test = train(model, X_train, X_test, save_path=save_path, device=device, epochs=3, batch_size=32, kfolds=10)
-
-    
+    losses, results_df = train(model, X_train, X_test, save_path=save_path, device=device, epochs=3, batch_size=32, kfolds=10)
 
     # save losses
     np.save(save_path / "losses.npy", losses)
-    np.save(save_path / "checkpoint_losses_train.npy", checkpoint_losses_train)
-    np.save(save_path / "checkpoint_losses_test.npy", checkpoint_losses_test)
+    results_df.to_csv(save_path / "results.csv", index=False)
+
+    # find the epoch and fold with the lowest validation loss
+    best_epoch = results_df.groupby("fold")["valid_loss"].idxmin().values[0]
+    best_fold = results_df.iloc[best_epoch]["fold"]
+
+    # load best model
+    model.load_state_dict(torch.load(f"{save_path}/fold_{best_fold}_epoch_{best_epoch+1}.pth"))
+    test_results = test(model, X_test, y_test, cid_test, mlb, epoch=best_epoch, kfold=best_fold, save_path=save_path)
+
 
     # save mlb
     with open(save_path / "mlb.pkl", "wb") as f:
