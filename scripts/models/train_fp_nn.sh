6e9ceb5c57fa2f5a0f34dc99e05e48ae26513bdd
Clay Kosonocky <90992096+kosonocky@users.noreply.github.com>
2023-08-16 09:19:02 -0500

    Reduced to 5fold cv

--- scripts/models/train_fp_nn.sh
+++ scripts/models/train_fp_nn.sh
@@ -1,23 +1,23 @@
 python -u train_fp_nn.py --d_hidden_1 0\
     --d_hidden_2 0\
     --d_hidden_3 0\
-    --k_folds 10\
+    --k_folds 5\
     --n_epochs 10 | tee -a log_fp_nn.txt
 
 python -u train_fp_nn.py --d_hidden_1 1796\
     --d_hidden_2 0\
     --d_hidden_3 0\
-    --k_folds 10\
+    --k_folds 5\
     --n_epochs 10 | tee -a log_fp_nn.txt
 
 python -u train_fp_nn.py --d_hidden_1 1880\
     --d_hidden_2 1712\
     --d_hidden_3 0\
-    --k_folds 10\
+    --k_folds 5\
     --n_epochs 10 | tee -a log_fp_nn.txt
 
 python -u train_fp_nn.py --d_hidden_1 1922\
     --d_hidden_2 1796\
     --d_hidden_3 1670\
-    --k_folds 10\
+    --k_folds 5\
     --n_epochs 10 | tee -a log_fp_nn.txt
